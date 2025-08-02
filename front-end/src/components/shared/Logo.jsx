17820365b9c6f159dee50b526f0a448f0e7697d4
Rajugit07 <rajupradhani7750@gmail.com>
2025-08-02 16:59:04 +0530

    shared compo comm

--- front-end/src/components/shared/Logo.jsx
+++ front-end/src/components/shared/Logo.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+// import React from "react";
+// import { Link } from "react-router-dom";
 
-const Logo = () => {
-    return (
-        <div>
-            <Link to="/">
-                <img src="./src/assets/logo.png" alt="logo" className="w-14" />
-            </Link>
-        </div>
-    );
-};
+// const Logo = () => {
+//     return (
+//         <div>
+//             <Link to="/">
+//                 <img src="./" alt="logo" className="w-14" />
+//             </Link>
+//         </div>
+//     );
+// };
 
-export default Logo;
+// export default Logo;
